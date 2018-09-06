b2310c6c562f16ab03eca70bf77d7f4f73fd97e7
gebele <gebele@in-silico.ch>
2018-09-06 10:50:27 +0200

    updated jre and rserve source

--- lazar-development/start.sh
+++ lazar-development/start.sh
@@ -2,6 +2,16 @@
 
 sudo /usr/bin/mongod &
 R CMD Rserve --vanilla & 
-ruby $HOME/lazar/test/all.rb
-#cd $HOME/lazar-gui &&
-#unicorn -p 8088 -E production
+
+# fetch and load database content
+#if [ ! -d "$HOME/dump" ]; then
+#  wget https://dump.in-silico.ch/dump.tar.gz
+#  tar xfvz dump.tar.gz
+#  mongorestore
+#fi
+
+# run tests
+#ruby $HOME/lazar/test/all.rb
+
+cd $HOME/lazar-gui &&
+unicorn -p 8088 -E production
